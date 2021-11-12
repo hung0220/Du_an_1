ebaaaa7effccc0868294b8a0c0c66cfcdc78b81a
NguyenHuuHung <nguyenhuuhung070220@gmail.com>
2021-11-12 16:41:13 +0700

    màn hinh vi tri

--- app/src/main/java/com/example/du_an_1/MainActivity.java
+++ app/src/main/java/com/example/du_an_1/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.yeucauvitri);
+        setContentView(R.layout.activity_main_login);
     }
 }
