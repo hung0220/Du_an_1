a36e4936e17fbf6408bb3529c71a0cd3304688f4
Hiro <jinokudai721@gmail.com>
2021-11-13 21:24:57 +0700

    Toast

--- app/src/main/java/com/example/du_an_1/MainActivity_Login.java
+++ app/src/main/java/com/example/du_an_1/MainActivity_Login.java
@@ -3,6 +3,7 @@ package com.example.du_an_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity_Login extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity_Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
