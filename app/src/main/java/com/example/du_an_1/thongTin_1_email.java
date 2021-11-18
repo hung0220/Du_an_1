da7c08fcd5d8b1c506eedebc81a55fdc54f2c216
NguyenHuuHung <nguyenhuuhung070220@gmail.com>
2021-11-18 08:21:36 +0700

    sửa from của hiếu

--- app/src/main/java/com/example/du_an_1/thongTin_1_email.java
+++ app/src/main/java/com/example/du_an_1/thongTin_1_email.java
@@ -17,7 +17,7 @@ public class thongTin_1_email extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.thong_tin_1_email);
+        setContentView(R.layout.thong_tin_1_email_sdt);
 //        Tiếp tục
         tiepTuc1 = findViewById(R.id.btnTT1);
         tiepTuc1.setOnClickListener(new View.OnClickListener() {
