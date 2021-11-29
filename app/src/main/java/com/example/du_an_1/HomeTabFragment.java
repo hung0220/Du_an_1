cfe60295036d179ef97b7597397d0ec3273c473d
Punpun <kendynhok0305@gmail.com>
2021-11-29 14:36:53 +0700

    them

--- app/src/main/java/com/example/du_an_1/HomeTabFragment.java
+++ app/src/main/java/com/example/du_an_1/HomeTabFragment.java
@@ -1,16 +1,152 @@
 package com.example.du_an_1;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.LinearInterpolator;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DiffUtil;
 
-public class HomeTabFragment extends Fragment {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.CardStackListener;
+import com.yuyakaido.android.cardstackview.CardStackView;
+import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.StackFrom;
+import com.yuyakaido.android.cardstackview.SwipeableMethod;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeTabFragment extends AppCompatActivity {
+    private static final String TAG = "HomeTabFragment";
+    private CardStackLayoutManager manager;
+    private CardStackAdapter adapter;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.home_tab_fragment, container, false);
-        return root ;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.home_tab_fragment);
+
+        CardStackView cardStackView = findViewById(R.id.card_stack_view);
+         manager = new CardStackLayoutManager(HomeTabFragment.this, new CardStackListener() {
+            @Override
+            public void onCardDragging(Direction direction, float ratio) {
+                Log.d(TAG, "onCardDragging: d=" + direction.name() + " ratio=" + ratio);
+            }
+
+            @Override
+            public void onCardSwiped(Direction direction) {
+                Log.d(TAG, "onCardSwiped: p=" + manager.getTopPosition() + " d=" + direction);
+                if (direction == Direction.Right){
+                    Toast.makeText(HomeTabFragment.this, "Direction Right", Toast.LENGTH_SHORT).show();
+                }
+                if (direction == Direction.Top){
+                    Toast.makeText(HomeTabFragment.this, "Direction Top", Toast.LENGTH_SHORT).show();
+                }
+                if (direction == Direction.Left){
+                    Toast.makeText(HomeTabFragment.this, "Direction Left", Toast.LENGTH_SHORT).show();
+                }
+                if (direction == Direction.Bottom){
+                    Toast.makeText(HomeTabFragment.this, "Direction Bottom", Toast.LENGTH_SHORT).show();
+                }
+
+                // Paginating
+                if (manager.getTopPosition() == adapter.getItemCount() - 5){
+                    paginate();
+                }
+
+            }
+
+            @Override
+            public void onCardRewound() {
+                Log.d(TAG, "onCardRewound: " + manager.getTopPosition());
+            }
+
+            @Override
+            public void onCardCanceled() {
+                Log.d(TAG, "onCardRewound: " + manager.getTopPosition());
+            }
+
+            @Override
+            public void onCardAppeared(View view, int position) {
+                TextView tv = view.findViewById(R.id.item_name);
+                Log.d(TAG, "onCardAppeared: " + position + ", nama: " + tv.getText());
+            }
+
+            @Override
+            public void onCardDisappeared(View view, int position) {
+                TextView tv = view.findViewById(R.id.item_name);
+                Log.d(TAG, "onCardAppeared: " + position + ", nama: " + tv.getText());
+            }
+        });
+        manager.setStackFrom(StackFrom.None);
+        manager.setVisibleCount(3);
+        manager.setTranslationInterval(8.0f);
+        manager.setScaleInterval(0.95f);
+        manager.setSwipeThreshold(0.3f);
+        manager.setMaxDegree(20.0f);
+        manager.setDirections(Direction.FREEDOM);
+        manager.setCanScrollHorizontal(true);
+        manager.setSwipeableMethod(SwipeableMethod.Manual);
+        manager.setOverlayInterpolator(new LinearInterpolator());
+        adapter = new CardStackAdapter(addList());
+        cardStackView.setLayoutManager(manager);
+        cardStackView.setAdapter(adapter);
+        cardStackView.setItemAnimator(new DefaultItemAnimator());
+
+        Menu_Nav();
+    }
+    private void paginate() {
+        List<ItemModelDulieu> old = adapter.getItems();
+        List<ItemModelDulieu> baru = new ArrayList<>(addList());
+        CardStackCallback callback = new CardStackCallback(old, baru);
+        DiffUtil.DiffResult hasil = DiffUtil.calculateDiff(callback);
+        adapter.setItems(baru);
+        hasil.dispatchUpdatesTo(adapter);
+    }
+
+    private List<ItemModelDulieu> addList() {
+        List<ItemModelDulieu> items = new ArrayList<>();
+        items.add(new ItemModelDulieu(R.drawable.bao, "Bảo", "16", "Đăk Lăk"));
+        items.add(new ItemModelDulieu(R.drawable.trung, "Trung", "20", "Đăk Nông"));
+        items.add(new ItemModelDulieu(R.drawable.toan, "Toàn", "20", "Đăk Lăk"));
+        items.add(new ItemModelDulieu(R.drawable.thai, "Thái", "19", "Đăk Lăk"));
+        return items;
+    }
+    public void Menu_Nav(){
+        BottomNavigationView bottomNavigationView   = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.Home_fragment);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                switch (item.getItemId()){
+                    case  R.id.Home_fragment:
+                        return true;
+                    case  R.id.Ib_fragment:
+                        startActivity(new Intent(getApplicationContext(),IbTabFragment.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case  R.id.Iam_fragment:
+                        startActivity(new Intent(getApplicationContext(),IamTabFragment.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/du_an_1/IbTabFragment.java
+++ app/src/main/java/com/example/du_an_1/IbTabFragment.java
@@ -1,16 +1,47 @@
 package com.example.du_an_1;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.MenuItem;
+import android.view.WindowManager;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class IbTabFragment extends Fragment {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
+
+
+public class IbTabFragment extends AppCompatActivity {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.ib_tab_fragment, container, false);
-        return root ;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.ib_tab_fragment);
+        Menu_Nav();
+    }
+    public void Menu_Nav(){
+        BottomNavigationView bottomNavigationView   = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.Ib_fragment);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                switch (item.getItemId()){
+                    case  R.id.Home_fragment:
+                        startActivity(new Intent(getApplicationContext(),HomeTabFragment.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case  R.id.Ib_fragment:
+                        return true;
+                    case  R.id.Iam_fragment:
+                        startActivity(new Intent(getApplicationContext(),IamTabFragment.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
